02c4a4118a69f05d787243251f60890a2f09656a
chy <cheny539@gmail.com>
2021-05-04 18:39:29 +0800

    test

--- model/fusion_esim.py
+++ model/fusion_esim.py
@@ -194,7 +194,7 @@ class Bert(nn.Module):
         d = 512 - r.size(1)
         c = c[:, :d]
         input_ids = torch.cat((c, r[:, 1:]), dim=1)
-        input_len = c.size(1) + r_len - 1
+        input_len = c.size(1) + r.size(1) - 1
         attn_mask = get_mask_from_seq_lens(input_len)
         token_ids = torch.cat((torch.zeros(c.size()[:2]),
                                torch.ones(r.size(0), r.size(1)-1)), dim=1).long().cuda()
@@ -203,7 +203,6 @@ class Bert(nn.Module):
        #     raise AssertionError
        # # input_ids, attn_mask, token_ids =
         print(input_ids.size(), attn_mask.size(), token_ids.size())
-        exit()
         output = self.BERT(input_ids=input_ids,
                            attention_mask=attn_mask,
                            token_type_ids=token_ids).last_hidden_state
