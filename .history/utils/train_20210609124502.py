e8246819d88819464828ebcc1953d0db2585000c
chy <cheny539@gmail.com>
2021-06-09 12:45:47 +0800

    bugs

--- .history/utils/train_20210609124502.py
+++ .history/utils/train_20210609124502.py
@@ -179,7 +179,7 @@ class LSTMTrainer(Trainer):
         batch["esim_data"] = data["esim_data"]
         batch = self.batch_bert_data(batch, data, 0)
         eva_lg_e, eva_lg_b = self.model(batch)
-        loss = self.crit(eva_lg_e, torch.tensor([1] * self.batch_size).to(self.device))
+        loss = self.crit(eva_lg_e, torch.tensor([1] * self.batch_size).to(self.device) 
         prob = nn.functional.softmax(eva_lg_e, dim=1)[:, 1].unsqueeze(1)
         total_loss += loss.item()
         for idx, b_sample in enumerate(b_neg):
