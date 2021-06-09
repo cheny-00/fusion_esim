f9b2dc55ebf4b6ec1ee3e8f770cc6d75a3d9baae
chy <m10709818@gapps.ntust.edu.tw>
2021-06-09 14:56:03 +0800

    merge

--- model/distilbert_post_train_model.py
+++ model/distilbert_post_train_model.py
@@ -204,9 +204,6 @@ class ModelOutput(OrderedDict):
         Convert self to a tuple containing all the attributes/keys that are not ``None``.
         """
         return tuple(self[k] for k in self.keys())
-<<<<<<< HEAD
-=======
-
 
 @dataclass
 class BertForPreTrainingOutput(ModelOutput):
